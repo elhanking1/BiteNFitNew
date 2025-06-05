121a40bdd1ccf02dfbdb7cba31327eba367400be
elhanking1 <kingelhan2@gmail.com>
2025-06-05 17:12:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-
 function buttonClick() {
-  console.log("Download button clicked!");
+  alert("A new version of the application is available.\nPlease download and install it.");
+
+  const apkUrl = "https://github.com/elhanking1/BiteNFitNew/releases/download/v4.0/BiteNFitv4.apk";
+
+  // If inside Android WebView, open via external browser
+  if (navigator.userAgent.includes("wv") || navigator.userAgent.includes("Android")) {
+    window.location.href = `intent://${apkUrl.replace(/^https?:\/\//, '')}#Intent;scheme=https;package=com.android.chrome;end`;
+  } else {
+    window.open(apkUrl, "_blank");
+  }
 }
