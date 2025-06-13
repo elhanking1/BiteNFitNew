f5148e792d275ab22b6903874b2a17aef9cd2576
elhanking1 <kingelhan2@gmail.com>
2025-06-14 01:05:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function buttonClick() {
-  const apkUrl = "https://github.com/elhanking1/BiteNFitNew/releases/download/v4.0/BiteNFitv4.apk";
+  const apkUrl = "https://github.com/elhanking1/BiteNFitNew/releases/download/v4.0/BiteNFitV4.1.apk";
   window.open(apkUrl, '_blank'); // Opens in browser
 }
